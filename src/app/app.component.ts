7bd3c6324c212325df6af1e4bad14feda6d6b654
Raushan Ranjan <jacksparrow14358@gmail.com>
2022-02-14 01:42:03 +0530

    netcore assignment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,99 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import {Observable} from 'rxjs';
+import { FormControl, FormGroup, NgForm, Validators } from '@angular/forms';
+import {COMMA, ENTER} from '@angular/cdk/keycodes';
+import  * as carList  from './car.json';
+import  * as carPart  from './car-parts.json';
+import {map, startWith} from 'rxjs/operators';
+import { MatChipInputEvent } from '@angular/material/chips';
+import { MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
+
+
+interface Cars{
+  brand:string;
+}
+
+interface CarPart{
+  'List-of-auto-parts':string;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit  {
   title = 'car-registration';
+  carsList: any = (carList as any).default;
+  carPart: any = (carPart as any).default;
+  selectedPart: string[] = [];
+  @ViewChild('carPartRef',{static: false}) carPartRef: ElementRef<HTMLInputElement> | any;
+  
+  name: FormControl= new FormControl('',[Validators.required,Validators.maxLength(75),Validators.pattern('^[a-zA-Z]*$')]);
+  cars: FormControl = new FormControl('', Validators.required);
+  carParts: FormControl = new FormControl('',Validators.required);
+  colorCar: FormControl = new FormControl('#000000',Validators.required);
+  filteredOptions: Observable<Cars[]> | any = []
+  filterCarParts: Observable<Cars[]> | any = []
+  separatorKeysCodes: number[] = [ENTER, COMMA];
+  constructor(){
+    this.filterCarParts = this.carParts.valueChanges.pipe(
+      startWith(null),
+      map((fruit: any | null) => (fruit ? this._filterPart(fruit) : this.carPart.slice()))
+    );
+  }
+  ngOnInit(): void {    
+    this.filteredOptions = this.cars.valueChanges.pipe(
+      startWith(''),
+      map(value => (typeof value === 'string' ? value : value.brand)),
+      map(name => (name ? this._filterCar(name) : this.carsList.slice())),
+    );
+  
+  }
+
+
+  private _filterCar(value: string): string[] {
+   const filterValue = value.toLowerCase();
+    return this.carsList.filter((option:any) => option.brand.toLowerCase().includes(filterValue));
+  }
+
+  private _filterPart(value: string): string[] {
+   const filterValue = value.toLowerCase();
+    return this.carPart.filter((option:any) => option['List-of-auto-parts'].toLowerCase().includes(filterValue));
+  }
+
+  displayFn(user: Cars): string {
+    return user && user.brand ? user.brand : '';
+  }
+
+  add(event: MatChipInputEvent): void {
+    const value = (event.value || '').trim();
+    if (value) {
+      this.selectedPart.push(value);
+    }
+    event.chipInput!.clear();
+    this.carParts.setValue(null);
+  }
+
+  remove(value: string): void {
+    const index = this.selectedPart.indexOf(value);
+    if (index >= 0) {
+      this.selectedPart.splice(index, 1);
+    }
+  }
+
+  selected(event: MatAutocompleteSelectedEvent): void {
+    this.selectedPart.push(event.option.viewValue);
+    this.carPartRef.nativeElement.value = '';
+    this.carParts.setValue(null);
+  }
+
+  onSubmit(){
+    console.log(this.name)
+    console.log(this.carParts)
+    console.log(this.colorCar)
+
+  }
+ 
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,18 @@
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import {MatCardModule} from '@angular/material/card';
+import {MatSelectModule} from '@angular/material/select';
+import {MatChipsModule} from '@angular/material/chips';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [
@@ -10,7 +20,18 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    MatAutocompleteModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    NgbModule,
+    MatCardModule,
+    MatSelectModule,
+    MatChipsModule,
+    MatIconModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
